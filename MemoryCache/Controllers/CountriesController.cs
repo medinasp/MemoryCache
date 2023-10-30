16af43e2df1d850732a14c37bd0d0b0fd6972626
Enéias Medina <medinasp@gmail.com>
2023-10-30 20:01:08 -0300

    Commit 2

---
+++ MemoryCache/Controllers/CountriesController.cs
@@ -0,0 +1,64 @@
+﻿using MemoryCache.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MemoryCache.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountriesController : ControllerBase
+    {
+        private readonly IMemoryCache _memoryCache;
+        private const string COUNTRIES_KEY = "Countries";
+
+        public CountriesController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            const string restCountriesUrl = "https://restcountries.com/v3.1/all";
+
+            if (_memoryCache.TryGetValue(COUNTRIES_KEY, out List<CountryViewModel> countries))
+            {
+                return Ok(countries);
+            }
+
+            using (var httpClient = new HttpClient())
+            {
+                var response = await httpClient.GetAsync(restCountriesUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseData = await response.Content.ReadAsStringAsync();
+
+                    var options = new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    };
+
+                    // Deserialize the JSON into List<CountryInfo>
+                    countries = JsonSerializer.Deserialize<List<CountryViewModel>>(responseData, options);
+
+                    // Store in memory cache
+                    var memoryCacheEntryOptions = new MemoryCacheEntryOptions
+                    {
+                        AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(3600),
+                        SlidingExpiration = TimeSpan.FromSeconds(1200),
+                    };
+
+                    _memoryCache.Set(COUNTRIES_KEY, countries, memoryCacheEntryOptions);
+
+                    return Ok(countries);
+                }
+
+                return BadRequest("Failed to fetch data from the API.");
+            }
+        }
+    }
+}
---
+++ MemoryCache/Model/CountryViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace MemoryCache.Model
+{
+    public class CountryViewModel
+    {
+        public NameData Name { get; set; }
+        public object Capital { get; set; }
+        public string Region { get; set; }
+        public Dictionary<string, string> Languages { get; set; }
+
+        public class NameData
+        {
+            public string Common { get; set; }
+        }
+    }
+}
